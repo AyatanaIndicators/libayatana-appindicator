89ebfc6d19ec1acc39e5eb271955b6c50cdbbe35
Ted Gould <ted@canonical.com>
2009-10-13 22:22:38 -0500

    Getting some functions so that we can link happy like.

--- src/custom-service.c
+++ src/custom-service.c
@@ -1,6 +1,13 @@
 #include "notification-item-client.h"
-#include "notification-watcher-server.h"
 
+void _notification_watcher_server_register_service (void) { };
+void _notification_watcher_server_registered_services (void) { };
+void _notification_watcher_server_protocol_version (void) { };
+void _notification_watcher_server_register_notification_host (void) { };
+void _notification_watcher_server_is_notification_host_registered (void) { };
+
+#include "notification-watcher-server.h"
+ 
 int
 main (int argc, char ** argv)
 {
