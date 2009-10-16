1101075695054df49e96d815fec28b4e9ea0287a
Ted Gould <ted@canonical.com>
2009-10-16 12:33:31 -0500

    Building a basic prototype of the API for the lib.

--- src/libcustomindicator/custom-indicator.h
+++ src/libcustomindicator/custom-indicator.h
@@ -13,6 +13,9 @@ G_BEGIN_DECLS
 #define IS_CUSTOM_INDICATOR_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE ((klass), CUSTOM_INDICATOR_TYPE))
 #define CUSTOM_INDICATOR_GET_CLASS(obj)  (G_TYPE_INSTANCE_GET_CLASS ((obj), CUSTOM_INDICATOR_TYPE, CustomIndicatorClass))
 
+typedef int custom_indicator_category_t;
+typedef int custom_indicator_status_t;
+
 typedef struct _CustomIndicator      CustomIndicator;
 typedef struct _CustomIndicatorClass CustomIndicatorClass;
 
@@ -24,7 +27,30 @@ struct _CustomIndicator {
 	GObject parent;
 };
 
-GType custom_indicator_get_type (void);
+/* GObject Stuff */
+GType                           custom_indicator_get_type           (void);
+
+/* Set properties */
+void                            custom_indicator_set_id             (CustomIndicator * ci,
+                                                                     const gchar * id);
+void                            custom_indicator_set_category       (CustomIndicator * ci,
+                                                                     custom_indicator_category_t category);
+void                            custom_indicator_set_status         (CustomIndicator * ci,
+                                                                     custom_indicator_status_t status);
+void                            custom_indicator_set_icon           (CustomIndicator * ci,
+                                                                     const gchar * icon_name);
+void                            custom_indicator_set_attention_icon (CustomIndicator * ci,
+                                                                     const gchar * icon_name);
+void                            custom_indicator_set_menu           (CustomIndicator * ci,
+                                                                     void * menu);
+
+/* Get properties */
+const gchar *                   custom_indicator_get_id             (CustomIndicator * ci);
+custom_indicator_category_t     custom_indicator_get_category       (CustomIndicator * ci);
+custom_indicator_status_t       custom_indicator_get_status         (CustomIndicator * ci);
+const gchar *                   custom_indicator_get_icon           (CustomIndicator * ci);
+const gchar *                   custom_indicator_get_attention_icon (CustomIndicator * ci);
+void *                          custom_indicator_get_menu           (CustomIndicator * ci);
 
 G_END_DECLS
 
