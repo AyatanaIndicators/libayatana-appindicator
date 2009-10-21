10e95506d81335d394b9cd955b7728b528c0fe4e
Ted Gould <ted@canonical.com>
2009-10-21 09:23:21 -0500

    Woot!  We can build a custom indicator, and it exists.

--- tests/test-libcustomindicator.c
+++ tests/test-libcustomindicator.c
@@ -2,6 +2,25 @@
 #include <glib.h>
 #include <glib-object.h>
 
+#include <libcustomindicator/custom-indicator.h>
+
+void
+test_libcustomindicator_init (void)
+{
+	CustomIndicator * ci = CUSTOM_INDICATOR(g_object_new(CUSTOM_INDICATOR_TYPE, NULL));
+	g_assert(ci != NULL);
+	g_object_unref(G_OBJECT(ci));
+}
+
+void
+test_libcustomindicator_props_suite (void)
+{
+	g_test_add_func ("/indicator-custom/libcustomindicator/init", test_libcustomindicator_init);
+
+
+	return;
+}
+
 gint
 main (gint argc, gchar * argv[])
 {
@@ -9,6 +28,8 @@ main (gint argc, gchar * argv[])
 	g_test_init(&argc, &argv, NULL);
 
 	/* Test suites */
+	test_libcustomindicator_props_suite();
+
 
 	return g_test_run ();
 }
