dc43af9a9e3aa1965bf3d9994f8733cdaf588912
Ted Gould <ted@canonical.com>
2009-10-21 11:40:23 -0500

    Checking all the set functions.

--- tests/test-libcustomindicator.c
+++ tests/test-libcustomindicator.c
@@ -5,6 +5,28 @@
 #include <libcustomindicator/custom-indicator.h>
 
 void
+test_libcustomindicator_init_set_props (void)
+{
+	CustomIndicator * ci = CUSTOM_INDICATOR(g_object_new(CUSTOM_INDICATOR_TYPE, NULL));
+	g_assert(ci != NULL);
+
+	custom_indicator_set_id(ci, "my-id");
+	custom_indicator_set_category(ci, CUSTOM_INDICATOR_CATEGORY_APPLICATION_STATUS);
+	custom_indicator_set_status(ci, CUSTOM_INDICATOR_STATUS_ON);
+	custom_indicator_set_icon(ci, "my-name");
+	custom_indicator_set_attention_icon(ci, "my-attention-name");
+
+	g_assert(!g_strcmp0("my-id", custom_indicator_get_id(ci)));
+	g_assert(!g_strcmp0("my-name", custom_indicator_get_icon(ci)));
+	g_assert(!g_strcmp0("my-attention-name", custom_indicator_get_attention_icon(ci)));
+	g_assert(custom_indicator_get_status(ci) == CUSTOM_INDICATOR_STATUS_ON);
+	g_assert(custom_indicator_get_category(ci) == CUSTOM_INDICATOR_CATEGORY_APPLICATION_STATUS);
+
+	g_object_unref(G_OBJECT(ci));
+	return;
+}
+
+void
 test_libcustomindicator_init_with_props (void)
 {
 	CustomIndicator * ci = CUSTOM_INDICATOR(g_object_new(CUSTOM_INDICATOR_TYPE, 
@@ -40,6 +62,7 @@ test_libcustomindicator_props_suite (void)
 {
 	g_test_add_func ("/indicator-custom/libcustomindicator/init",        test_libcustomindicator_init);
 	g_test_add_func ("/indicator-custom/libcustomindicator/init_props",  test_libcustomindicator_init_with_props);
+	g_test_add_func ("/indicator-custom/libcustomindicator/init_set_props",  test_libcustomindicator_init_set_props);
 
 
 	return;
