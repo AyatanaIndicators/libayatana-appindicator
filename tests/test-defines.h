bca9b7ee96632e7c89092184c174b45722e0511c
Ted Gould <ted@canonical.com>
2009-10-22 09:20:36 -0500

    Adding in two little client server binaries for testing the custom indicator.

---
+++ tests/test-defines.h
@@ -0,0 +1,7 @@
+
+#define  TEST_ID                   "my-id"
+#define  TEST_ICON_NAME            "my-icon-name"
+#define  TEST_ATTENTION_ICON_NAME  "my-attention-icon-name"
+#define  TEST_STATE                CUSTOM_INDICATOR_STATE_ACTIVE
+#define  TEST_CATEGORY             CUSTOM_INDICATOR_CATEGORY_APPLICATION_STATUS
+
---
+++ tests/test-libcustomindicator-dbus-client.c
@@ -0,0 +1,11 @@
+
+#include <glib.h>
+#include <libcustomindicator/custom-indicator.h>
+#include "test-defines.h"
+
+gint
+main (gint argc, gchar * argv[])
+{
+
+	return 0;
+}
