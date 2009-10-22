83a3d84ea6e0bb8a9ce7927b08658307becbb186
Ted Gould <ted@canonical.com>
2009-10-22 09:46:17 -0500

    Fleshing out the server to setup a simple custom indicator.

--- tests/test-libcustomindicator-dbus-server.c
+++ tests/test-libcustomindicator-dbus-server.c
@@ -1,11 +1,41 @@
 
+#include <dbus/dbus-glib.h>
+#include <dbus/dbus-glib-lowlevel.h>
 #include <glib.h>
 #include <libcustomindicator/custom-indicator.h>
 #include "test-defines.h"
 
+static GMainLoop * mainloop = NULL;
+
+gboolean
+kill_func (gpointer userdata)
+{
+	g_main_loop_quit(mainloop);
+	return FALSE;
+}
+
 gint
 main (gint argc, gchar * argv[])
 {
+	g_type_init();
+
+	g_debug("DBus ID: %s", dbus_connection_get_server_id(dbus_g_connection_get_connection(dbus_g_bus_get(DBUS_BUS_SESSION, NULL))));
+
+	CustomIndicator * ci = CUSTOM_INDICATOR(g_object_new(CUSTOM_INDICATOR_TYPE, 
+	                                                     "id", TEST_ID,
+	                                                     "category", TEST_CATEGORY,
+	                                                     "status", TEST_STATE,
+	                                                     "icon-name", TEST_ICON_NAME,
+	                                                     "attention-icon-name", TEST_ATTENTION_ICON_NAME,
+	                                                     NULL));
+
+	g_timeout_add_seconds(2, kill_func, NULL);
+
+	mainloop = g_main_loop_new(NULL, FALSE);
+	g_main_loop_run(mainloop);
+
+	g_object_unref(G_OBJECT(ci));
+	g_debug("Quiting");
 
 	return 0;
 }
