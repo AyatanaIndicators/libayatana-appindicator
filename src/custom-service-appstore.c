8903841582ea26b4987fbe04b5a5545d8d65f729
Ted Gould <ted@canonical.com>
2009-10-28 15:12:46 -0500

    Boom!  Now we're implementing dbus.

--- src/custom-service-appstore.c
+++ src/custom-service-appstore.c
@@ -4,6 +4,18 @@
 
 #include "custom-service-appstore.h"
 
+/* DBus Prototypes */
+static gboolean _custom_service_server_get_applications (CustomServiceAppstore * appstore, GArray ** apps, gpointer user_data);
+
+static gboolean _notification_watcher_server_register_service (CustomServiceAppstore * appstore, const gchar * service, gpointer user_data);
+static gboolean _notification_watcher_server_registered_services (CustomServiceAppstore * appstore, GArray ** apps, gpointer user_data);
+static gboolean _notification_watcher_server_protocol_version (CustomServiceAppstore * appstore, char ** version, gpointer user_data);
+static gboolean _notification_watcher_server_register_notification_host (CustomServiceAppstore * appstore, const gchar * host, gpointer user_data);
+static gboolean _notification_watcher_server_is_notification_host_registered (CustomServiceAppstore * appstore, gboolean * haveHost, gpointer user_data);
+
+#include "custom-service-server.h"
+#include "notification-watcher-server.h"
+
 typedef struct _CustomServiceAppstorePrivate CustomServiceAppstorePrivate;
 
 struct _CustomServiceAppstorePrivate {
@@ -55,3 +67,47 @@ custom_service_appstore_finalize (GObject *object)
 	G_OBJECT_CLASS (custom_service_appstore_parent_class)->finalize (object);
 	return;
 }
+
+/* DBus Interface */
+static gboolean
+_custom_service_server_get_applications (CustomServiceAppstore * appstore, GArray ** apps, gpointer user_data)
+{
+
+	return FALSE;
+}
+
+static gboolean
+_notification_watcher_server_register_service (CustomServiceAppstore * appstore, const gchar * service, gpointer user_data)
+{
+
+	return FALSE;
+}
+
+static gboolean
+_notification_watcher_server_registered_services (CustomServiceAppstore * appstore, GArray ** apps, gpointer user_data)
+{
+
+	return FALSE;
+}
+
+static gboolean
+_notification_watcher_server_protocol_version (CustomServiceAppstore * appstore, char ** version, gpointer user_data)
+{
+
+	return FALSE;
+}
+
+static gboolean
+_notification_watcher_server_register_notification_host (CustomServiceAppstore * appstore, const gchar * host, gpointer user_data)
+{
+
+	return FALSE;
+}
+
+static gboolean
+_notification_watcher_server_is_notification_host_registered (CustomServiceAppstore * appstore, gboolean * haveHost, gpointer user_data)
+{
+
+	return FALSE;
+}
+
--- src/custom-service.c
+++ src/custom-service.c
@@ -1,12 +1,6 @@
+
 #include "notification-item-client.h"
-
-void _notification_watcher_server_register_service (void) { };
-void _notification_watcher_server_registered_services (void) { };
-void _notification_watcher_server_protocol_version (void) { };
-void _notification_watcher_server_register_notification_host (void) { };
-void _notification_watcher_server_is_notification_host_registered (void) { };
-
-#include "notification-watcher-server.h"
+#include "custom-service-appstore.h"
  
 int
 main (int argc, char ** argv)
