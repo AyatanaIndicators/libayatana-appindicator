f2c4a15e281e697bd6222efe80432014f0cef32f
Ted Gould <ted@canonical.com>
2009-10-28 15:55:28 -0500

    Building our app store

--- src/custom-service.c
+++ src/custom-service.c
@@ -1,10 +1,19 @@
 
 #include "notification-item-client.h"
 #include "custom-service-appstore.h"
+
+static GMainLoop * mainloop = NULL;
+static CustomServiceAppstore * appstore = NULL;
  
 int
 main (int argc, char ** argv)
 {
+	g_type_init();
+
+	appstore = CUSTOM_SERVICE_APPSTORE(g_object_new(CUSTOM_SERVICE_APPSTORE_TYPE, NULL));
+
+	mainloop = g_main_loop_new(NULL, FALSE);
+	g_main_loop_run(mainloop);
 
 	return 0;
 }
