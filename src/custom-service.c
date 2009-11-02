3d1c4c46fd56576b228bc59dc7b3af3bf82db642
Ted Gould <ted@canonical.com>
2009-11-02 17:19:25 -0600

    Boom, make us an indicator service.

--- src/custom-service.c
+++ src/custom-service.c
@@ -1,15 +1,18 @@
 
+#include "libindicator/indicator-service.h"
 #include "notification-item-client.h"
 #include "custom-service-appstore.h"
 
 static GMainLoop * mainloop = NULL;
 static CustomServiceAppstore * appstore = NULL;
+static IndicatorService * service = NULL;
  
 int
 main (int argc, char ** argv)
 {
 	g_type_init();
 
+	service = indicator_service_new("org.ayatana.indicator.custom");
 	appstore = CUSTOM_SERVICE_APPSTORE(g_object_new(CUSTOM_SERVICE_APPSTORE_TYPE, NULL));
 
 	mainloop = g_main_loop_new(NULL, FALSE);
