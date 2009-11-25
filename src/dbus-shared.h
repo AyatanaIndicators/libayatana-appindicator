ad2afb8723f171a9a3d952761172f3ba1be0d808
Ted Gould <ted@gould.cx>
2009-11-24 21:55:10 -0600

    Changing all the dbus names.

--- src/dbus-shared.h
+++ src/dbus-shared.h
@@ -1,10 +1,10 @@
 
-#define INDICATOR_CUSTOM_DBUS_ADDR        "org.ayatana.indicator.custom"
-#define INDICATOR_CUSTOM_DBUS_OBJ         "/org/ayatana/indicator/custom/service"
-#define INDICATOR_CUSTOM_DBUS_IFACE       "org.ayatana.indicator.custom.service"
+#define INDICATOR_CUSTOM_DBUS_ADDR        "org.ayatana.indicator.application"
+#define INDICATOR_CUSTOM_DBUS_OBJ         "/org/ayatana/indicator/application/service"
+#define INDICATOR_CUSTOM_DBUS_IFACE       "org.ayatana.indicator.application.service"
 
-#define NOTIFICATION_WATCHER_DBUS_OBJ     "/org/ayatana/indicator/custom/NotificationWatcher"
-#define NOTIFICATION_WATCHER_DBUS_IFACE   "org.ayatana.indicator.custom.NotificationWatcher"
+#define NOTIFICATION_WATCHER_DBUS_OBJ     "/org/ayatana/indicator/application/NotificationWatcher"
+#define NOTIFICATION_WATCHER_DBUS_IFACE   "org.ayatana.indicator.application.NotificationWatcher"
 
-#define NOTIFICATION_ITEM_DBUS_IFACE      "org.ayatana.indicator.custom.NotificationItem"
+#define NOTIFICATION_ITEM_DBUS_IFACE      "org.ayatana.indicator.application.NotificationItem"
 
