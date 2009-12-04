c9670e625cd4a7d04709e4d64b77be80f5f70799
Cody Russell <crussell@canonical.com>
2009-12-04 11:06:41 -0600

    Remove the menu test

--- tests/test-libappindicator-dbus-client.c
+++ tests/test-libappindicator-dbus-client.c
@@ -7,16 +7,16 @@ Copyright 2009 Canonical Ltd.
 Authors:
     Ted Gould <ted@canonical.com>
 
-This program is free software: you can redistribute it and/or modify it 
-under the terms of the GNU General Public License version 3, as published 
+This program is free software: you can redistribute it and/or modify it
+under the terms of the GNU General Public License version 3, as published
 by the Free Software Foundation.
 
-This program is distributed in the hope that it will be useful, but 
-WITHOUT ANY WARRANTY; without even the implied warranties of 
-MERCHANTABILITY, SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR 
+This program is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranties of
+MERCHANTABILITY, SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR
 PURPOSE.  See the GNU General Public License for more details.
 
-You should have received a copy of the GNU General Public License along 
+You should have received a copy of the GNU General Public License along
 with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
@@ -33,7 +33,7 @@ static int propcount = 0;
 static void
 check_propcount (void)
 {
-	if (propcount >= 6) {
+	if (propcount >= 5) {
 		g_main_loop_quit(mainloop);
 	}
 	return;
@@ -175,33 +175,6 @@ prop_attention_icon_name_cb (DBusGProxy * proxy, DBusGProxyCall * call, void * d
 	return;
 }
 
-static void
-prop_menu_cb (DBusGProxy * proxy, DBusGProxyCall * call, void * data)
-{
-	propcount++;
-
-	GError * error = NULL;
-	GValue value = {0};
-
-	if (!dbus_g_proxy_end_call(proxy, call, &error, G_TYPE_VALUE, &value, G_TYPE_INVALID)) {
-		g_warning("Getting menu object failed: %s", error->message);
-		g_error_free(error);
-		passed = FALSE;
-		check_propcount();
-		return;
-	}
-
-	if (g_strcmp0(TEST_OBJECT, g_value_get_string(&value))) {
-		g_debug("Property menu object Returned: FAILED");
-		passed = FALSE;
-	} else {
-		g_debug("Property menu object Returned: PASSED");
-	}
-
-	check_propcount();
-	return;
-}
-
 gboolean
 kill_func (gpointer userdata)
 {
@@ -269,13 +242,6 @@ main (gint argc, gchar * argv[])
 	                         NULL, NULL,
 	                         G_TYPE_STRING, "org.ayatana.indicator.application.NotificationItem",
 	                         G_TYPE_STRING, "AttentionIconName",
-	                         G_TYPE_INVALID);
-	dbus_g_proxy_begin_call (props,
-	                         "Get",
-	                         prop_menu_cb,
-	                         NULL, NULL,
-	                         G_TYPE_STRING, "org.ayatana.indicator.application.NotificationItem",
-	                         G_TYPE_STRING, "Menu",
 	                         G_TYPE_INVALID);
 
 	g_timeout_add_seconds(2, kill_func, NULL);
