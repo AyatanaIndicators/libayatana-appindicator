8045abdf6b0390572dbeb1a224431ec4c3e3355e
Ted Gould <ted@gould.cx>
2010-01-15 01:01:11 -0600

    Setting the proper type for the GValue of an 'o'

--- src/application-service-appstore.c
+++ src/application-service-appstore.c
@@ -656,8 +656,8 @@ _application_service_server_get_applications (ApplicationServiceAppstore * appst
 		g_value_unset(&value);
 
 		/* DBus Object */
-		g_value_init(&value, G_TYPE_STRING);
-		g_value_set_string(&value, ((Application *)listpntr->data)->dbus_object);
+		g_value_init(&value, DBUS_TYPE_G_OBJECT_PATH);
+		g_value_set_static_boxed(&value, ((Application *)listpntr->data)->dbus_object);
 		g_value_array_append(values, &value);
 		g_value_unset(&value);
 
