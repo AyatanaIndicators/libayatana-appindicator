d415cb6768ad49b66b313336f2191f92579549e9
Ted Gould <ted@gould.cx>
2010-01-27 10:32:53 -0600

    Changing strings in the test suite as well.

--- tests/test-defines.h
+++ tests/test-defines.h
@@ -23,8 +23,8 @@ with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define  TEST_ICON_NAME            "my-icon-name"
 #define  TEST_ATTENTION_ICON_NAME  "my-attention-icon-name"
 #define  TEST_STATE                APP_INDICATOR_STATUS_ACTIVE
-#define  TEST_STATE_S              "active"
+#define  TEST_STATE_S              "Active"
 #define  TEST_CATEGORY             APP_INDICATOR_CATEGORY_APPLICATION_STATUS
-#define  TEST_CATEGORY_S           "application-status"
+#define  TEST_CATEGORY_S           "ApplicationStatus"
 #define  TEST_OBJECT               "/an/object/path/to/use"
 
