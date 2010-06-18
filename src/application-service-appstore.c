ce25d4c9ac44237bb1f38576e99de3533489bd74
Ted Gould <ted@gould.cx>
2010-06-17 22:16:01 -0500

    Use class ref and unref to allocate it if need be.

--- src/application-service-appstore.c
+++ src/application-service-appstore.c
@@ -243,13 +243,17 @@ get_all_properties_cb (DBusGProxy * proxy, GHashTable * properties, GError * err
 static AppIndicatorStatus
 string_to_status(const gchar * status_string)
 {
-	GEnumClass * klass = G_ENUM_CLASS(g_type_class_peek_static(APP_INDICATOR_TYPE_INDICATOR_STATUS));
+	GEnumClass * klass = G_ENUM_CLASS(g_type_class_ref(APP_INDICATOR_TYPE_INDICATOR_STATUS));
 	g_return_val_if_fail(klass != NULL, APP_INDICATOR_STATUS_PASSIVE);
 
 	GEnumValue * val = g_enum_get_value_by_nick(klass, status_string);
 	g_return_val_if_fail(val != NULL, APP_INDICATOR_STATUS_PASSIVE);
 
-	return (AppIndicatorStatus)val->value;
+	AppIndicatorStatus retval = (AppIndicatorStatus)val->value;
+
+	g_type_class_unref(klass);
+
+	return retval;
 }
 
 /* A small helper function to get the position of an application
