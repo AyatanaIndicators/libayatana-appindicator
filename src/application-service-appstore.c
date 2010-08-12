efd7be1cf7c80abc13c6d3bb3bc99cef18b8db7b
Ted Gould <ted@gould.cx>
2010-08-11 19:59:38 -0500

    Adding a count for the number of approvers that have approved the application.

--- src/application-service-appstore.c
+++ src/application-service-appstore.c
@@ -85,6 +85,7 @@ struct _Application {
 	gchar * label;
 	gchar * guide;
 	gboolean currently_free;
+	gint approved_by;
 };
 
 #define APPLICATION_SERVICE_APPSTORE_GET_PRIVATE(o) \
@@ -734,6 +735,7 @@ application_service_appstore_application_add (ApplicationServiceAppstore * appst
 	app->label = NULL;
 	app->guide = NULL;
 	app->currently_free = FALSE;
+	app->approved_by = 0;
 
 	/* Get the DBus proxy for the NotificationItem interface */
 	GError * error = NULL;
