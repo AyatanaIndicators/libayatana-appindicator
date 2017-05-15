4dd25fc0cf2eaa190663daf0d7457a39a1dbafc2
Mike Gabriel <mike.gabriel@das-netzwerkteam.de>
2017-05-15 08:55:32 +0200

    src/dbus-shared.h: Fix copy+paste flaw for INDICATOR_APPLICATION_DBUS_OBJ.

--- src/dbus-shared.h
+++ src/dbus-shared.h
@@ -21,7 +21,7 @@ with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
 #define INDICATOR_APPLICATION_DBUS_ADDR        "org.ayatana.indicator.application"
-#define INDICATOR_APPLICATION_DBUS_OBJ         "/org.ayatana.indicator.application/service"
+#define INDICATOR_APPLICATION_DBUS_OBJ         "/org/ayatana/indicator/application/service"
 #define INDICATOR_APPLICATION_DBUS_IFACE       "org.ayatana.indicator.application.service"
 
 #define NOTIFICATION_WATCHER_DBUS_ADDR    "org.kde.StatusNotifierWatcher"
@@ -32,4 +32,3 @@ with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define NOTIFICATION_ITEM_DEFAULT_OBJ     "/StatusNotifierItem"
 
 #define NOTIFICATION_APPROVER_DBUS_IFACE  "org.ayatana.StatusNotifierApprover"
-
